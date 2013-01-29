895eb73c30efd07d1f3e5e3f2b6d78d87df2747b
Elymar <jozzua@gmail.com>
2013-01-29 16:04:50 +0800

    Finished static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static Pages" do
+
+  describe "Home Page" do
+    it "should have the h1 'Home'" do
+      visit "/static_pages/home"
+      page.should have_selector("h1", :text => "Home")
+    end
+
+    it "should have the title 'Home'" do
+      visit "/static_pages/home"
+      page.should have_selector('title', :text => "YumHunt | Home")
+    end
+
+  end
+
+  describe "Help Page" do
+    it "should have the h1 'Help'" do
+      visit "/static_pages/help"
+      page.should have_selector("h1", :text =>"Help")
+    end
+    it "should have the title 'Help'" do
+      visit "/static_pages/help"
+      page.should have_selector("title", :text =>"YumHunt | Help")
+    end
+
+  end
+
+  describe "About Page" do
+    it "should have the h1 'About'" do
+      visit "/static_pages/about"
+      page.should have_selector("h1", :text => "About")
+    end
+    it "should have the title 'About'" do
+      visit "/static_pages/about"
+      page.should have_selector("title", :text =>"YumHunt | About")
     end
   end
+
 end
