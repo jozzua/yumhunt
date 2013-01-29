f67ce4cd4eb96d64bb70767f4df7d748ac7bb0c7
Elymar <jozzua@gmail.com>
2013-01-29 14:15:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yumhunt::Application.config.secret_token = '14ddb70c7491318a11b0b42a208368e777255f7b8c66c6bbc726a9083f947febce5676fdb5c98a5f4e9048a932255c00a66068931c4165446c3e743c6f4383b0'
