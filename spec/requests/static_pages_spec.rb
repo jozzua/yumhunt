21dedc3eed3e72d2508a6e38e6af16a728bcf688
Elymar <jozzua@gmail.com>
2013-01-30 08:22:26 +0800

    Fixes to guard file and layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,12 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do
       visit "/static_pages/home"
-      page.should have_selector('title', :text => "YumHunt | Home")
+      page.should have_selector('title', :text => "YumHunt" )
+    end
+
+    it "should not have a custom page title" do
+      visit "/static_pages/home"
+      page.should_not have_selector('title',:text => "| Home")
     end
 
   end
