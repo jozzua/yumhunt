c160851b0a6b5790a4921346f0826e763ebba358
Elymar <jozzua@gmail.com>
2013-02-01 09:36:11 +0800

    Password confirmation added but still failing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,15 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "example.email.com")}
+  before { @user = User.new(name: "Example User", email: "example.email.com", password:"foobar", password_confimation:"foobar")}
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " "}
@@ -59,6 +64,21 @@ describe User do
       user_with_same_email.save
     end
     it {should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before {@user.password = @user.password_confirmation = " " }
+    it {should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation": do
+    before {@user.password_confirmation = "mismatch"}
+    it {should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before {@user.password_confirmation = nil }
+    it { should_not be_valid }
   end
 
 end
